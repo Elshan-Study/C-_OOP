ecb20bf525da4b99ec864272c1b6b47f81f5aef0
Elshan-Study <isaye_ee51@itstep.edu.az>
2025-03-03 12:08:18 +0400

    some changes

--- C++_OOP/C++lesson_16.cpp
+++ C++_OOP/C++lesson_16.cpp
@@ -3,30 +3,60 @@
 class User
 {
 private:
-    char name[20];
-    int age;
-    char email[50];
-    char password[50];
+    char _name[20];
+    int _age;
+    char _email[50];
+    char _password[50];
+
+    void setPassword(const char* newPassword) {
+        if (newPassword != nullptr && strlen(newPassword) >= 8 && strlen(newPassword) <= 16 && newPassword != _password)
+        {
+            strncpy_s(_password, newPassword, sizeof(newPassword));
+            _password[strlen(newPassword) - 1] = '\0';
+        }
+    }
 
 public:
 
     User() 
     {    /* default constructor*/
   
-        strncpy_s(name, "Unknown", sizeof(name));
-        age = 0;
-        strncpy_s(email, "Unknown", sizeof(email));
-        strncpy_s(password, "Unknown", sizeof(password));
+        strncpy_s(_name, "Unknown", sizeof(_name));
+        _age = 0;
+        strncpy_s(_email, "Unknown", sizeof(_email));
+        strncpy_s(_password, "Unknown", sizeof(_password));
     }
 
-    int GetAge() { return age; }
-    void SetAge(int age) { this->age = age; }
+    User(const char* name)
+
+
+    const char* getName() const { return _name; }
+
+    int getAge() const { return _age; } /*const запрещает внутри метода менять данные*/
+    const char* getEmail() const { return _email; }
+    const char* getPassword() const { return _password; }
+
+    void setName(const char* newName)
+    {
+        if (newName && _name != "/0")
+        {
+            strncpy_s(_name, "Unknown", sizeof(newName));
+        }
+    }
+
+    void setAge(int newAge)
+    {
+        if (newAge > 0)
+        {
+            _age = newAge;
+        }
+    }
 };
 
-void display_info(User user) {
+void displayInfo(const User& user) {
 
-    std::cout << "Name: " << name;
-    std::cout << "\nAge: " << age;
+    std::cout << "Name: " << user.getName();
+    std::cout << "\nAge: " << user.getAge();
     std::cout << "\nEmail: " << email;
     std::cout << "\nPassword: " << password;
     std::cout << "\n";
