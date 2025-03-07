72ec01b25c16297972a256606711cc0da5d21421
Elshan Isayev <elshanisayev378@gmail.com>
2025-03-07 15:32:42 +0400

    Set(), PushBack(),  Pop(), Remove() added to topic 17

--- C++_OOP/C++lesson_16.cpp
+++ C++_OOP/C++lesson_16.cpp
@@ -1,85 +1,85 @@
-#define _CRT_SECURE_NO_WARNINGS
-#include <iostream>
-
-class User
-{
-private:
-    char _name[20];
-    int _age;
-    char _email[50];
-    char _password[50];
-
-    void setPassword(const char* newPassword)
-    {
-        if (newPassword != nullptr && strlen(newPassword) >= 8 && strlen(newPassword) <= 16
-            && newPassword != _password)
-        {
-            strncpy(_password, newPassword, strlen(newPassword));
-            _password[strlen(newPassword) - 1] = '\0';
-        }
-    }
-public:
-    User() // default constructor
-    {
-        strncpy(_name, "Unknown", sizeof(_name));
-        _age = 0;
-        strncpy(_email, "Unknown", sizeof(_email));
-        strncpy(_password, "Unknown", sizeof(_password));
-    }
-
-
-    // constructor with parameters
-    User(const char* name, const int age, const char* email, const char* password)
-    {
-        setName(name);
-        setAge(age);
-        setEmail(email);
-        setPassword(password);
-    }
-
-    const char* getName() const { return _name; }
-    int getAge() const { return _age; }
-    const char* getEmail() const { return _email; }
-    const char* getPassword() const { return _password; }
-
-    void setName(const char* newName)
-    {
-        if (newName && _name[0] != '\0')
-        {
-            strncpy(_name, newName, sizeof(_name));
-            _name[strlen(newName) - 1] = '\0';
-        }
-    }
-
-    void setAge(const int newAge)
-    {
-        if (newAge > 0)
-        {
-            _age = newAge;
-        }
-    }
-
-    void setEmail(const char* newEmail)
-    {
-        if (newEmail && _email[0] != '\0')
-        {
-            strncpy(_email, newEmail, sizeof(_email));
-            _email[strlen(newEmail) - 1] = '\0';
-        }
-    }
-
-
-
-};
-
-void DisplayInfo(const User& user)
-{
-    std::cout << "Name: " << user.getName()
-        << "\nAge: " << user.getAge()
-        << "\nEmail: " << user.getEmail()
-        << "\nPassword: " << user.getPassword()
-        << "\n";
-}
+//#define _CRT_SECURE_NO_WARNINGS
+//#include <iostream>
+//
+//class User
+//{
+//private:
+//    char _name[20];
+//    int _age;
+//    char _email[50];
+//    char _password[50];
+//
+//    void setPassword(const char* newPassword)
+//    {
+//        if (newPassword != nullptr && strlen(newPassword) >= 8 && strlen(newPassword) <= 16
+//            && newPassword != _password)
+//        {
+//            strncpy(_password, newPassword, strlen(newPassword));
+//            _password[strlen(newPassword) - 1] = '\0';
+//        }
+//    }
+//public:
+//    User() // default constructor
+//    {
+//        strncpy(_name, "Unknown", sizeof(_name));
+//        _age = 0;
+//        strncpy(_email, "Unknown", sizeof(_email));
+//        strncpy(_password, "Unknown", sizeof(_password));
+//    }
+//
+//
+//    // constructor with parameters
+//    User(const char* name, const int age, const char* email, const char* password)
+//    {
+//        setName(name);
+//        setAge(age);
+//        setEmail(email);
+//        setPassword(password);
+//    }
+//
+//    const char* getName() const { return _name; }
+//    int getAge() const { return _age; }
+//    const char* getEmail() const { return _email; }
+//    const char* getPassword() const { return _password; }
+//
+//    void setName(const char* newName)
+//    {
+//        if (newName && _name[0] != '\0')
+//        {
+//            strncpy(_name, newName, sizeof(_name));
+//            _name[strlen(newName) - 1] = '\0';
+//        }
+//    }
+//
+//    void setAge(const int newAge)
+//    {
+//        if (newAge > 0)
+//        {
+//            _age = newAge;
+//        }
+//    }
+//
+//    void setEmail(const char* newEmail)
+//    {
+//        if (newEmail && _email[0] != '\0')
+//        {
+//            strncpy(_email, newEmail, sizeof(_email));
+//            _email[strlen(newEmail) - 1] = '\0';
+//        }
+//    }
+//
+//
+//
+//};
+//
+//void DisplayInfo(const User& user)
+//{
+//    std::cout << "Name: " << user.getName()
+//        << "\nAge: " << user.getAge()
+//        << "\nEmail: " << user.getEmail()
+//        << "\nPassword: " << user.getPassword()
+//        << "\n";
+//}
 
 //int main()
 //{
--- C++_OOP/C++lesson_18.cpp
+++ C++_OOP/C++lesson_18.cpp
@@ -1,76 +1,76 @@
-#include <iostream>
-
-class SuperVector
-{
-private:
-	int* _data;
-	size_t _size;
-	size_t _capacity;
-public:
-	SuperVector() : _data(nullptr), _size(0), _capacity(0) {}
-	explicit SuperVector(size_t size) : _size(size), _capacity(size)
-	{
-		_data = new int[_capacity]();
-	}
-	SuperVector(const SuperVector& other) : _size(other._size), _capacity(other._capacity) /*copying constructor*/
-	{
-		_data = new int[_capacity];
-		for (size_t i = 0; i < _size; i++)
-		{
-			_data[i] = other._data[i];
-		}
-	}
-
-	/*move constructor*/
-	SuperVector(SuperVector&& other) noexcept : _data(other._data), _size(other._size), _capacity(other._capacity)
-	{
-		std::cout << "move constructor" << std::endl;
-		other._data = nullptr;
-		other._size = 0;
-		other._capacity = 0;
-	}
-
-	int Get(size_t index) const
-	{
-		if (index <= _size && index >= 0)
-		{
-			return _data[index];
-		}
-
-		return 0;
-	}
-
-	size_t Size() const
-	{
-		return _size;
-	}
-
-	~SuperVector()
-	{
-		delete[] _data;
-	}
-};
-
-void DisplayVector(const SuperVector& vector)
-{
-	for (size_t i = 0; i < vector.Size(); i++)
-	{
-		std::cout << vector.Get(i) << " ";
-	}
-}
-
-int main()
-{
-	/*SuperVector vector(10);
-	std::cout << "Before\n";
-	DisplayVector(vector);
-	SuperVector vector_2 = std::move(vector);
-	std::cout << "After\n";
-	DisplayVector(vector);
-	DisplayVector(vector_2);*/
-
-	SuperVector vector = 5;
-	std::cout << vector.Size() << "\n";
-
-	return 0;
-}
+//#include <iostream>
+//
+//class SuperVector
+//{
+//private:
+//	int* _data;
+//	size_t _size;
+//	size_t _capacity;
+//public:
+//	SuperVector() : _data(nullptr), _size(0), _capacity(0) {}
+//	explicit SuperVector(size_t size) : _size(size), _capacity(size)
+//	{
+//		_data = new int[_capacity]();
+//	}
+//	SuperVector(const SuperVector& other) : _size(other._size), _capacity(other._capacity) /*copying constructor*/
+//	{
+//		_data = new int[_capacity];
+//		for (size_t i = 0; i < _size; i++)
+//		{
+//			_data[i] = other._data[i];
+//		}
+//	}
+//
+//	/*move constructor*/
+//	SuperVector(SuperVector&& other) noexcept : _data(other._data), _size(other._size), _capacity(other._capacity)
+//	{
+//		std::cout << "move constructor" << std::endl;
+//		other._data = nullptr;
+//		other._size = 0;
+//		other._capacity = 0;
+//	}
+//
+//	int Get(size_t index) const
+//	{
+//		if (index <= _size && index >= 0)
+//		{
+//			return _data[index];
+//		}
+//
+//		return 0;
+//	}
+//
+//	size_t Size() const
+//	{
+//		return _size;
+//	}
+//
+//	~SuperVector()
+//	{
+//		delete[] _data;
+//	}
+//};
+//
+//void DisplayVector(const SuperVector& vector)
+//{
+//	for (size_t i = 0; i < vector.Size(); i++)
+//	{
+//		std::cout << vector.Get(i) << " ";
+//	}
+//}
+//
+//int main()
+//{
+//	/*SuperVector vector(10);
+//	std::cout << "Before\n";
+//	DisplayVector(vector);
+//	SuperVector vector_2 = std::move(vector);
+//	std::cout << "After\n";
+//	DisplayVector(vector);
+//	DisplayVector(vector_2);*/
+//
+//	SuperVector vector = 5;
+//	std::cout << vector.Size() << "\n";
+//
+//	return 0;
+//}
