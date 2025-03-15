b0de6d44cdc1193b8c0621be158c53dbfc7af9e0
Elshan Isayev <elshanisayev378@gmail.com>
2025-03-15 11:16:30 +0400

    for (auto i : numbers)

--- C++_OOP/C++lesson_22.cpp
+++ C++_OOP/C++lesson_22.cpp
@@ -18,10 +18,23 @@
 //{
 //};
 
-//int main()
-//{
-//    Box<int> box(5);
-//    Box<double> box_2(5.34);
-//   
-//    return 0;
-//}
+
+template <typename T>
+void print_numbers(std::initializer_list<T> numbers)
+{
+    for (auto i : numbers)
+    {
+        std::cout << i << " ";
+    }
+    std::cout << "\n";
+}
+
+int main()
+{
+   /* Box<int> box(5);
+    Box<double> box_2(5.34);*/
+
+    print_numbers<int>({ 1, 2, 3, 4, 5, 6 });
+   
+    return 0;
+}
