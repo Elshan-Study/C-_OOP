294eead9f8cd3e98e5af3476c5c12d1ca6ba56e9
Elshan-Study <isaye_ee51@itstep.edu.az>
2025-03-19 11:09:33 +0400

    shared_ptr

--- C++_OOP/C++lesson_23.cpp
+++ C++_OOP/C++lesson_23.cpp
@@ -2,11 +2,12 @@
 #include <memory>
 
 //unique_ptr
-
+//shared_ptr
 
 class Biba
 {
 public:
+	int test = 5;
 	Biba() { std::cout << "Biba" << std::endl; }
 	~Biba() { std::cout << "~Biba" << std::endl; }
 
@@ -16,14 +17,22 @@ public:
 	}
 };
 
-//int main()
-//{
-//	std::unique_ptr<Biba> biba_ptr = std::make_unique<Biba>();
-//	biba_ptr->sayHello();
-//
-//	std::unique_ptr<Biba> biba_ptr2 = std::move(biba_ptr);
-//
-//	std::unique_ptr<int[]> boba_ptr = std::make_unique<int[]>(5);
-//
-//	return 0;
-//}
+int main()
+{
+	std::unique_ptr<Biba> biba_ptr = std::make_unique<Biba>();
+	biba_ptr->sayHello();
+
+	std::unique_ptr<Biba> biba_ptr2 = std::move(biba_ptr);
+
+	std::unique_ptr<int[]> boba_ptr = std::make_unique<int[]>(5);
+
+	std::shared_ptr<Biba> biba_shared_ptr = std::make_shared<Biba>();
+	std::shared_ptr<Biba> biba_shared_ptr2 = biba_shared_ptr;
+	std::shared_ptr<Biba> biba_shared_ptr3 = biba_shared_ptr2;
+	std::shared_ptr<Biba> biba_shared_ptr4 = biba_shared_ptr3;
+
+	biba_shared_ptr2->test = 2;
+	std::cout << biba_shared_ptr4->test;
+
+	return 0;
+}
--- C++_OOP/main.cpp
+++ C++_OOP/main.cpp
@@ -12,67 +12,67 @@ void DisplayVector(const SuperVector& vector)
 	}
 };
 
-int main()
-{
-	/*homework 19*/
-
-	/*SuperVector v(3);
-	SuperVector v2(3);
-	v[0] = 1;
-	v[1] = 3;
-	v[2] = 5;
-	v2[0] = 2;
-	v2[1] = 4;
-	v2[2] = 0;
-	SuperVector v3(v/v2);
-
-	DisplayVector(v3);
-	std::cout << std::endl;*/
-
-	/*homework 20*/
-	//SuperVector v1(3);
-	//SuperVector v2(4);
-
-	//std::cout << "Vector 1: \n";
-	//std::cin >> v1; 
-	//std::cout << v1;
-	///*std::cout << "Vector 2: \n";
-	//std::cin >> v2;
-	//std::cout << v2;*/
-	//std::cout << v1--;
-	//std::cout << v1;
-
-	/*homework 21*/
-	//ZooWorker caretaker("John Doe", 5);
-	//Enclosure enclosure("Savannah", 3, caretaker);
-	//Mammal* animal_1 = new Mammal("Lion", 5, 150.0, 1);
-	//Mammal* animal_2 = new Mammal("Elephant", 10, 500.0, 0);
-	//Reptile* animal_3 = new Reptile("Crocodile", 10, 100.0, 0);
-	//enclosure.addAnimal(animal_1);
-	//enclosure.addAnimal(animal_2);
-	//enclosure.addAnimal(animal_3);
-
-	////std::cout << enclosure;
-	///*enclosure.printEnclosureInfo();
-
-	//enclosure.removeAnimal(0);
-	//enclosure.printEnclosureInfo();*/
-
-	//Zoo zoo;
-	//zoo.addEnclosure(enclosure);
-	//std::cout << zoo;
-
-	/*homework 22*/
-
-	SuperVectorT <double> vectorT({ 11, 66, 55 });
-	SuperVectorT <double> vectorT2({ 5, 6.8, 45 });
-
-	SuperVectorT <double> vectorT3 = vectorT + vectorT2;
-	std::cout << vectorT3[5] << "\n";
-
-	vectorT += vectorT3;
-
-	std::cout << vectorT.Size();
-
-	return 0;
-}
+//int main()
+//{
+//	/*homework 19*/
+//
+//	/*SuperVector v(3);
+//	SuperVector v2(3);
+//	v[0] = 1;
+//	v[1] = 3;
+//	v[2] = 5;
+//	v2[0] = 2;
+//	v2[1] = 4;
+//	v2[2] = 0;
+//	SuperVector v3(v/v2);
+//
+//	DisplayVector(v3);
+//	std::cout << std::endl;*/
+//
+//	/*homework 20*/
+//	//SuperVector v1(3);
+//	//SuperVector v2(4);
+//
+//	//std::cout << "Vector 1: \n";
+//	//std::cin >> v1; 
+//	//std::cout << v1;
+//	///*std::cout << "Vector 2: \n";
+//	//std::cin >> v2;
+//	//std::cout << v2;*/
+//	//std::cout << v1--;
+//	//std::cout << v1;
+//
+//	/*homework 21*/
+//	//ZooWorker caretaker("John Doe", 5);
+//	//Enclosure enclosure("Savannah", 3, caretaker);
+//	//Mammal* animal_1 = new Mammal("Lion", 5, 150.0, 1);
+//	//Mammal* animal_2 = new Mammal("Elephant", 10, 500.0, 0);
+//	//Reptile* animal_3 = new Reptile("Crocodile", 10, 100.0, 0);
+//	//enclosure.addAnimal(animal_1);
+//	//enclosure.addAnimal(animal_2);
+//	//enclosure.addAnimal(animal_3);
+//
+//	////std::cout << enclosure;
+//	///*enclosure.printEnclosureInfo();
+//
+//	//enclosure.removeAnimal(0);
+//	//enclosure.printEnclosureInfo();*/
+//
+//	//Zoo zoo;
+//	//zoo.addEnclosure(enclosure);
+//	//std::cout << zoo;
+//
+//	/*homework 22*/
+//
+//	/*SuperVectorT <double> vectorT({ 11, 66, 55 });
+//	SuperVectorT <double> vectorT2({ 5, 6.8, 45 });
+//
+//	SuperVectorT <double> vectorT3 = vectorT + vectorT2;
+//	std::cout << vectorT3[5] << "\n";
+//
+//	vectorT += vectorT3;
+//
+//	std::cout << vectorT.Size();*/
+//
+//	return 0;
+//}
