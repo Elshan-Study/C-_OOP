97310ed7c509b14dc157a2c579dad4305cd214a8
Elshan-Study <isaye_ee51@itstep.edu.az>
2025-03-19 11:33:54 +0400

    weak_ptr

--- C++_OOP/C++lesson_23.cpp
+++ C++_OOP/C++lesson_23.cpp
@@ -3,23 +3,47 @@
 
 //unique_ptr
 //shared_ptr
+//weak_ptr
 
-class Biba
+//class Biba
+//{
+//public:
+//	int test = 5;
+//	Biba() { std::cout << "Biba" << std::endl; }
+//	~Biba() { std::cout << "~Biba" << std::endl; }
+//
+//	void sayHello()
+//	{
+//		std::cout << "Hello\n";
+//	}
+//};
+
+class B;
+
+class A
 {
 public:
-	int test = 5;
-	Biba() { std::cout << "Biba" << std::endl; }
-	~Biba() { std::cout << "~Biba" << std::endl; }
-
-	void sayHello()
+	std::weak_ptr<B> b_ptr;
+	~A()
 	{
-		std::cout << "Hello\n";
+		std::cout << "Destructing A\n";
 	}
 };
 
+class B
+{
+public:
+	std::shared_ptr<A> a_ptr;
+	~B()
+	{
+		std::cout << "Destructing B\n";
+	}
+};
+
+
 int main()
 {
-	std::unique_ptr<Biba> biba_ptr = std::make_unique<Biba>();
+	/*std::unique_ptr<Biba> biba_ptr = std::make_unique<Biba>();
 	biba_ptr->sayHello();
 
 	std::unique_ptr<Biba> biba_ptr2 = std::move(biba_ptr);
@@ -32,7 +56,13 @@ int main()
 	std::shared_ptr<Biba> biba_shared_ptr4 = biba_shared_ptr3;
 
 	biba_shared_ptr2->test = 2;
-	std::cout << biba_shared_ptr4->test;
+	std::cout << biba_shared_ptr4->test;*/
 
+
+	std::shared_ptr<A> a = std::make_shared<A>();
+	std::shared_ptr<B> b = std::make_shared<B>();
+
+	a->b_ptr = b;
+	b->a_ptr = a;
 	return 0;
 }
