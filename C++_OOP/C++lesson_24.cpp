3addef09a8f042455bc6e574c6cae4515016e885
Elshan-Study <isaye_ee51@itstep.edu.az>
2025-04-02 10:30:44 +0400

    Lesson 24: Polymorphism problems without Polymorphism

---
+++ C++_OOP/C++lesson_24.cpp
@@ -0,0 +1,77 @@
+#include <iostream>
+
+class Character
+{
+protected:
+	std::string _name;
+	int _hp;
+	int _stamina;
+public:
+	Character() : _name(""), _hp(0), _stamina(0) {};
+	Character(std::string name, int hp, int stamina) : _name(name), _hp(hp), _stamina(stamina) {};
+
+	void attack()
+	{
+		std::cout << "class Character \n";
+		std::cout << _name << " attacks " << _hp << " HP" << std::endl;
+	}
+
+	void defend()
+	{
+		std::cout << "class Character \n";
+		std::cout << _name << " defends " << _hp << " HP" << std::endl;
+	}
+};
+
+class Player : public Character
+{
+public:
+	Player() {};
+	Player(std::string name, int hp, int stamina) : Character(name, hp, stamina) {};
+
+	void attack()
+	{
+		std::cout << "class Player \n";
+		std::cout << _name << " attack " << _hp << " HP" << std::endl;
+	}
+
+	void defend()
+	{
+		std::cout << "class Player \n";
+		std::cout << _name << " defend " << _hp << " HP" << std::endl;
+	}
+};
+
+class Orc : public Character
+{
+public:
+	Orc() {};
+	Orc(std::string name, int hp, int stamina) : Character(name, hp, stamina) {};
+
+	void attack()
+	{
+		std::cout << "class Orc \n";
+		std::cout << _name << " attack " << _hp << " HP" << std::endl;
+	}
+
+	void defend()
+	{
+		std::cout << "class Orc \n";
+		std::cout << _name << " defend " << _hp << " HP" << std::endl;
+	}
+};
+
+
+int main()
+{
+	/*Player player("Player1", 10, 20);
+	player.attack();*/
+
+	Character** character = new Character*[2];
+	character[0] = new Player("Player1", 10, 20);
+	character[1] = new Orc("Orc1", 50, 100);
+
+	character[0]->attack();
+
+	return 0;
+}
