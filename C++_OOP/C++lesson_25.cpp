35fcf0449a4f967e72961c8cc1547eaeba8aec70
Elshan-Study <isaye_ee51@itstep.edu.az>
2025-04-04 11:21:28 +0400

    Abstract class

--- C++_OOP/C++lesson_25.cpp
+++ C++_OOP/C++lesson_25.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#define PI 3,14159265
 
 /*early building*/
 //class A
@@ -74,3 +75,70 @@
 //	/*void Biba() override {}; final запретил переопределение*/
 //};
 
+class Shape
+{
+public:
+	virtual double area() const		= 0; /*pure virtual method*/
+	virtual double perimetr() const = 0; /*pure virtual method*/
+
+	virtual ~Shape() = default;
+};
+
+class Rectangle : public Shape
+{
+private:
+	double _width;
+	double _height;
+
+public:
+	Rectangle() : _width(0), _height(0) {};
+	Rectangle(double width, double height) : _width(width), _height(height) {};
+
+	double area() const override
+	{
+		return _width * _height;
+	};
+
+	double perimetr() const
+	{
+		return 2 * (_width + _height);
+	}
+};
+
+class Circle : public Shape
+{
+private:
+	double _radius;
+
+public:
+	Circle() : _radius(0) {};
+	Circle(double radius) : _radius(radius) {};
+
+	double area() const override
+	{
+		return PI * (_radius * _radius);
+	};
+
+	double perimetr() const
+	{
+		return 2 * PI * _radius;
+	}
+};
+
+int main()
+{
+	/*Shape shape; prohibited by virtual method*/
+
+	Rectangle rec(4.0, 6.0);
+	Circle circle(5.0);
+
+	Shape* shapes[] = { &circle, &rec };
+
+	for (Shape* shape : shapes)
+	{
+		std::cout << "Area: " << shape->area() << std::endl;
+		std::cout << "Perimetr: " << shape->perimetr() << std::endl;
+	}
+
+	return 0;
+}
